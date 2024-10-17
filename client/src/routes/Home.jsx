4b5257d3a10a92b03aae67b962718667655d60cb
Daniel Hackworth <dan.hackworth.515@gmail.com>
2024-10-17 18:43:44 -0400

    projext init with cdha

---
+++ client/src/routes/Home.jsx
@@ -0,0 +1,19 @@
+
+    import React from 'react'
+    import '../App.css';
+    
+
+    const Home = () => {
+      return (
+        <>
+          <h1>Docs for AudioCloud Api</h1>
+          <p>post request the audio file to this url ""</p>
+          <p>response will be a url</p>
+          <p>easy peasy</p>
+          <p>keep files on the smaller side</p>
+        </>
+      )
+    }
+    
+    export default Home
+    
